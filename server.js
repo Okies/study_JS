14e2d038b75eafd5fec1cff4f6e1329a001c40f6
Okies <ja00800@nate.com>
2019-04-09 01:22:33 +0900

    code upload

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 app.listen(3000, () => console.log('Server started on port 3000 한글'));
 
 let deptList = [
     {
-        id: '2180',
-        name: '통합IS팀 정보기획담당'
+        deptId: '2180',
+        deptName: '통합IS팀 정보기획담당'
     }
 ];
 
@@ -19,8 +19,14 @@ let empList = [
     {
         id: '22210',
         name: '곽준창',
-        deptid: '2180',
-        deptname: '통합IS팀 정보기획담당'
+        deptId: '2180',
+        deptName: '통합IS팀 정보기획담당'
+    },
+    {
+      id: '12345',
+      name: '한승덕',
+      deptId: '2180',
+      deptName: '통합IS팀 정보기획담당'
     }
 ];
 
@@ -32,18 +38,34 @@ app.get('/emps', cors(), (req, res) => res.json(empList));
 
 app.get('/depts/:id', cors(), (req, res) => {
     const id = parseInt(req.params.id, 10);
-    
+
     if (!id) {
         return res.status(400).json({ error: 'Incorrect id' });
     }
 
-    let dept = deptList.filter(dept => dept.id == id)[0];
+    let dept = deptList.filter(dept => dept.deptId == id)[0];
 
     if (!dept) {
         return res.status(404).json({ error: 'Unkown dept' });
     }
 
     return res.json(dept);
+});
+
+app.get('/emps/:id', cors(), (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    if (!id) {
+        return res.status(400).json({ error: 'Incorrect id' });
+    }
+
+    let emp = empList.filter(emp => emp.id == id)[0];
+
+    if (!emp) {
+        return res.status(404).json({ error: 'Unkown emp' });
+    }
+
+    return res.json(emp);
 });
 
 app.get('/depts/:id/emps', cors(), (req, res) => {
@@ -53,31 +75,59 @@ app.get('/depts/:id/emps', cors(), (req, res) => {
         return res.status(400).json({ error: 'Incorrect id' });
     }
 
-    let dept = deptList.filter(dept => dept.id == id)[0];
+    let dept = deptList.filter(dept => dept.deptId == id)[0];
 
     if (!dept) {
         return res.status(404).json({ error: 'Unkown dept' });
     }
 
-    return res.json(empList.filter(emp => emp.deptid=id));
+    return res.json(empList.filter(emp => emp.deptId==id));
 });
 
 app.post('/depts', cors(), (req, res) => {
-    const name = req.body.name || '';
-    const id = req.body.id || '';
+    const deptName = req.body.deptName || '';
+    const deptId = req.body.deptId || '';
 
-    if (!name.length) {
+    if (!deptName.length) {
         return res.status(400).json({ error: 'Incorrect name' });
-    } else if (!id.length) {
+    } else if (!deptId.length) {
         return res.status(400).json({ error: 'Incorrect id' });
     }
 
     const newDept = {
-        id: id,
-        name: name
+        deptId: deptId,
+        deptName: deptName
     };
 
     deptList.push(newDept);
 
     return res.status(201).json(newDept);
+});
+
+app.put('/emps/:id', cors(), (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  const deptId = req.body.deptId || '';
+  const deptName = req.body.deptName || '';
+
+  if (!id) {
+      return res.status(400).json({ error: 'Incorrect emp id' });
+  }
+
+  let emp = empList.filter(emp => emp.id == id)[0];
+
+  if (!emp) {
+      return res.status(404).json({ error: 'Unkown emp' });
+  }
+
+  if (!deptName.length) {
+      return res.status(400).json({ error: 'Incorrect name' });
+  } else if (!deptId.length) {
+      return res.status(400).json({ error: 'Incorrect id' });
+  }
+
+  emp.deptId = deptId;
+  emp.deptName = deptName;
+
+  return res.json(emp);
 });
